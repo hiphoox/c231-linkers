9030c2fee08a48b716559ebcd3ad51497bc7135b
Teran28 <teranaldo@comunidad.unam.mx>
2022-10-14 20:06:49 -0500

    Quitamos los mensajes de cada bandera

--- lib/nqcc.ex
+++ lib/nqcc.ex
@@ -6,7 +6,7 @@ defmodule Nqcc do
     "help" => "Prints this help",
     "asm" => "Shows the assembly code",
     "tl" => "Shows token list",
-    "aa" => "Shows AST Tree"
+    "ast" => "Shows AST Tree"
   }
 
   def main(args) do
@@ -19,32 +19,35 @@ defmodule Nqcc do
     OptionParser.parse(args, switches: [help: :boolean])
     OptionParser.parse(args, switches: [asm: :boolean])
     OptionParser.parse(args, switches: [tl: :boolean])
-    OptionParser.parse(args, switches: [aa: :boolean])
+    OptionParser.parse(args, switches: [ast: :boolean])
   end
+
+
+
+
 
   defp process_args({[help: true], _, _}) do
     print_help_message()
-  end
-
-  defp process_args({[asm: true], _, _}) do
-    print_asm_message()
-  end
-
-  defp process_args({[tl: true], _, _}) do
-    print_tl_message()
-  end
-
-  defp process_args({[aa: true], _, _}) do
-    print_aa_message()
   end
 
   defp process_args({_, [file_name], _}) do
     compile_file(file_name)
   end
 
+  defp process_args({[asm: true], [file_name], _}) do
+    asm_gen(file_name)
+  end
+
   defp process_args({[tl: true], [file_name], _}) do
     token_list_gen(file_name)
   end
+
+  defp process_args({[ast: true], [file_name], _}) do
+    tree_gen(file_name)
+  end
+
+
+
 
 
   defp compile_file(file_path) do
@@ -63,11 +66,25 @@ defmodule Nqcc do
   end
 
   defp token_list_gen(file_path) do
-    IO.puts("Token List: " <> file_path)
-    File.read!(file_path)
-    |> Lexer.scan_words()
-    |> IO.inspect(label: "\nLexer ouput")
+    doc = File.read!(file_path)
+    tokenslist = Lexer.scan_words()
   end
+
+  defp asm_gen(file_path) do
+
+
+
+  end
+
+  defp tree_gen(file_path) do
+
+
+  end
+
+
+
+
+
 
   defp print_help_message do
     IO.puts("\nnqcc --help file_name \n")
@@ -78,23 +95,7 @@ defmodule Nqcc do
     |> Enum.map(fn {command, description} -> IO.puts("  #{command} - #{description}") end)
   end
 
-  defp print_asm_message do
-    IO.puts("\nnqcc --asm file_name \n")
 
-    IO.puts("\nThe compiler shows the assembly code\n")
-  end
-
-  defp print_tl_message do
-    IO.puts("\nnqcc --tl file_name \n")
-
-    IO.puts("\nThe compiler shows the token list\n")
-  end
-
-  defp print_aa_message do
-    IO.puts("\nnqcc --aa file_name \n")
-
-    IO.puts("\nThe compiler shows the AST Tree\n")
-  end
 
 
 end
