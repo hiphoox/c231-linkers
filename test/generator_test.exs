270ae7827ec5f4c59c2a78e1be25777619a54431
CristianH2 <tyfre12345@gmail.com>
2022-12-21 15:10:05 -0600

    test generador

--- test/generator_test.exs
+++ test/generator_test.exs
@@ -3,7 +3,7 @@ defmodule GeneratorTest do
   doctest CodeGenerator
 
   setup_all do
-    {:ok,
+    {:ok, #return 2
     codigo0: """
         .section	.text.startup,"ax",@progbits
         .p2align 4
@@ -14,17 +14,52 @@ defmodule GeneratorTest do
         ret
         .section	.note.GNU-stack,"",@progbits
 
-    """,
+    """, #return 100
     codigo1: """
         .section	.text.startup,"ax",@progbits\n    .p2align 4\n    .globl  main
-    main:\n    endbr64\n    movl  $100, %eax\n    ret\n    .section	.note.GNU-stack,"",@progbits
+    main:\n    endbr64\n    movl  $100, %eax\n    ret
+        .section	.note.GNU-stack,"",@progbits
 
-    """,
+    """, #return 0
     codigo2: """
         .section	.text.startup,"ax",@progbits\n    .p2align 4\n    .globl  main
-    main:\n    endbr64\n    movl  $0, %eax\n    ret\n    .section	.note.GNU-stack,"",@progbits
+    main:\n    endbr64\n    movl  $0, %eax\n    ret
+        .section	.note.GNU-stack,"",@progbits
 
-    """
+    """, #return -2
+    codigo3: """
+        .section	.text.startup,"ax",@progbits\n    .p2align 4\n    .globl  main
+    main:\n    endbr64\n    movl  $2, %eax\n    neg	%eax\n    ret
+        .section	.note.GNU-stack,"",@progbits
+
+    """,  #return ~2
+    codigo4: """
+        .section	.text.startup,"ax",@progbits\n    .p2align 4\n    .globl  main
+    main:\n    endbr64\n    movl  $2, %eax\n    not	%eax\n    ret
+        .section	.note.GNU-stack,"",@progbits
+
+    """, #return !2
+    codigo5: """
+        .section	.text.startup,"ax",@progbits\n    .p2align 4\n    .globl  main
+    main:\n    endbr64\n    movl  $2, %eax\n    cmpl     $0, %eax\n    movl     $0, %eax
+        sete     %al\n    ret
+        .section	.note.GNU-stack,"",@progbits
+
+    """, #return -~!2
+    codigo6: """
+        .section	.text.startup,"ax",@progbits\n    .p2align 4\n    .globl  main
+    main:\n    endbr64\n    movl  $2, %eax\n    cmpl     $0, %eax\n    movl     $0, %eax
+        sete     %al\n    not	%eax\n    neg	%eax\n    ret
+        .section	.note.GNU-stack,"",@progbits
+
+    """, #return !~-2
+    codigo7: """
+        .section	.text.startup,"ax",@progbits\n    .p2align 4\n    .globl  main
+    main:\n    endbr64\n    movl  $2, %eax\n    neg	%eax\n    not	%eax
+        cmpl     $0, %eax\n    movl     $0, %eax\n    sete     %al\n    ret
+        .section	.note.GNU-stack,"",@progbits
+
+    """,
     }
 
   end
@@ -89,8 +124,11 @@ defmodule GeneratorTest do
   end
 
   test "Saltos de linea", state do
-      assert "int\nmain\n()\n{return 2;}" |> Sanitizer.sanitize_source() |> Lexer.scan_words()
-      |> Parser.parse_program()|> CodeGenerator.generate_code() ==
+      assert "int\nmain\n()\n{return 2;}"
+      |> Sanitizer.sanitize_source()
+      |> Lexer.scan_words()
+      |> Parser.parse_program()
+      |> CodeGenerator.generate_code() ==
       state[:codigo] or state[:codigo0]
   end
 
@@ -108,5 +146,71 @@ defmodule GeneratorTest do
       |> Lexer.scan_words()
       |> Parser.parse_program()
       |> CodeGenerator.generate_code() == state[:codigo0]
+  end
+
+  #------ Pruebas parte 2 -------
+  test "Operador negation", state do
+    code = """
+    int main() {
+      return -2;
+    }
+    """
+    assert code
+      |> Sanitizer.sanitize_source()
+      |> Lexer.scan_words()
+      |> Parser.parse_program()
+      |> CodeGenerator.generate_code() == state[:codigo3]
+  end
+
+  test "Operador bitwise complement", state do
+    code = """
+    int main() {
+      return ~2;
+    }
+    """
+    assert code
+      |> Sanitizer.sanitize_source()
+      |> Lexer.scan_words()
+      |> Parser.parse_program()
+      |> CodeGenerator.generate_code() == state[:codigo4]
+  end
+
+  test "Operador logical negation", state do
+    code = """
+    int main() {
+      return !2;
+    }
+    """
+    assert code
+      |> Sanitizer.sanitize_source()
+      |> Lexer.scan_words()
+      |> Parser.parse_program()
+      |> CodeGenerator.generate_code() == state[:codigo5]
+  end
+
+  test "Multiples operadores", state do
+    code = """
+    int main() {
+      return -~!2;
+    }
+    """
+    assert code
+      |> Sanitizer.sanitize_source()
+      |> Lexer.scan_words()
+      |> Parser.parse_program()
+      |> CodeGenerator.generate_code() == state[:codigo6]
+  end
+
+  test "Multiples operadores 2", state do
+    code = """
+    int main() {
+      return !~-2;
+    }
+    """
+    assert code
+      |> Sanitizer.sanitize_source()
+      |> Lexer.scan_words()
+      |> Parser.parse_program()
+      |> CodeGenerator.generate_code() == state[:codigo7]
   end
 end
