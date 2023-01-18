316b6f968440d4357d2b4ee45997817a6e85c339
CristianH2 <tyfre12345@gmail.com>
2023-01-18 15:51:29 -0600

    Correccion

--- test/nqcc_test.exs
+++ test/nqcc_test.exs
@@ -3,9 +3,9 @@ defmodule NqccTest do
   doctest Nqcc
 
   test "Pruebas nivel sistema" do
-    dir_aux = Path.absname("test/test_nora")
-    path_pruebas = Path.absname("test/test_nora/test_compiler.sh")
-    path_nqcc = Path.absname("nqcc")
+    #dir_aux = Path.absname("test/test_nora")
+    #path_pruebas = Path.absname("test/test_nora/test_compiler.sh")
+    #path_nqcc = Path.absname("nqcc")
     #{out,n}=System.cmd(path_pruebas, [path_nqcc, "1", "2"], cd: dir_aux)
 
     #{out,n}=System.cmd(path_pruebas, [path_nqcc, "3", "4"], cd: dir_aux)
